f592757f6c501879ea2fa48b0d0dbe6a408eea9c
Thomas <thomaswprice@msn.com>
2015-09-11 19:10:56 -0400

    Update assignments

--- logging/assignment.js
+++ logging/assignment.js
@@ -1,8 +1,8 @@
 window.assignments = {
 	"none": "None",
-	"guess1Lab": "Guessing Game Part 1 (Lab)",
-	"guess2HW": "Guessing Game Part 2 (Homework)",
-	"brickLab": "Brick Wall (Lab)",
-	"project": "Final Project",
+	"guess1Lab": "Guessing Game Part 1 (In Lab Activity 3)",
+	"guess2HW": "Guessing Game Part 2 (Homework 3)",
+	"guess3Lab": "Guessing Game Part 3 (In Lab Activity 4)",
+	"project": "SNAP Project (Project Deliverable 1)",
 	"test": "Testing",
 }
